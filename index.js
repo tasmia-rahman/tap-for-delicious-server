18ab401720c685a8166545b3f74390c2a42d9773
abrahon <mdabrahon926@gmail.com>
2023-01-20 01:38:11 +0600

    added database new name

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+
+require('dotenv').config();
+
+const app = express();
 
 
 const port = process.env.PORT || 5000;
