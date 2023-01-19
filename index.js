af1e0f5f6acac7554fca0e7f7d782a06a10c456e
tasmia-rahman <maliha1288@gmail.com>
2023-01-20 00:53:39 +0600

    restaurant registration backend done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,26 +11,33 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mpeq17q.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
-        const servicesCollection = client.db('deliciousRestaurant').collection('services');
+        const servicesCollection = client.db('TapForDeliciousDB').collection('services');
+        const sellersCollection = client.db('TapForDeliciousDB').collection('sellers');
 
-        app.get('/services', async(req, res) =>{
+        // Restaurants
+        app.get('/services', async (req, res) => {
             const query = {};
             const options = await servicesCollection.find(query).toArray();
             res.send(options);
         })
-        app.get('/services/:id', async(req, res) =>{
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await servicesCollection.findOne(query);
             res.send(service);
         })
-      
 
+        // Sellers
+        app.post('/sellers', async (req, res) => {
+            const seller = req.body;
+            seller.joinDate = Date();
+            const result = await sellersCollection.insertOne(seller);
+            res.send(result);
+        });
 
     }
     finally {
@@ -38,14 +45,12 @@ async function run() {
     }
 
 }
-run().catch(console.log)
-
-
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
-    res.send('tap restaurant server is running');
+    res.send('API running');
 })
 
 app.listen(port, () => {
-    console.log(`tap restaurant server running ${port}`);
+    console.log('Server is running on port', port);
 })
